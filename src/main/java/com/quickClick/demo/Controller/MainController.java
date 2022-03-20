c453e9bde7ec0f89d9c50f7f297463f8e5772de3
mriganktandon <sachitkapoor01@gmail.com>
2022-03-20 16:32:57 +0530

    merchant added

--- src/main/java/com/quickClick/demo/Controller/MainController.java
+++ src/main/java/com/quickClick/demo/Controller/MainController.java
@@ -2,8 +2,8 @@ package com.quickClick.demo.Controller;
 
 import com.quickClick.demo.Entity.MerchantEntity;
 import com.quickClick.demo.Entity.ProductEntity;
-import com.quickClick.demo.Pojo.Product;
-//<<<<<<< HEAD
+
+
 import com.quickClick.demo.Service.MerchantService;
 import com.quickClick.demo.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//=======
+
 import com.quickClick.demo.Repository.ProductSearch;
-//import com.quickClick.demo.Service.ProductService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.web.bind.annotation.*;
-//>>>>>>> 803826345933dfc4c751216e6010eea702d81553
+
 
 import javax.validation.Valid;
 import java.util.List;
@@ -32,14 +28,10 @@ public class MainController {
     @Autowired
     ProductService productService;
 
-//<<<<<<< HEAD
-//    @Autowired
-//    MerchantService merchantService;
-//
-//    @GetMapping("/products")
-//    public List<ProductEntity> getStudent(){
-//        return productService.getAllProducts();
-//=======
+
+    @Autowired
+    MerchantService merchantService;
+
     @GetMapping("/products")
     public List<ProductEntity> getAllProducts(){
 
@@ -52,10 +44,10 @@ public class MainController {
 
     }
 
-//    @GetMapping("/merchant/")
-//    public MerchantEntity getMerchantById(@RequestParam @Valid Integer id){
-//        return merchantService.getMerchantById(id);
-//    }
+    @GetMapping("/merchant")
+    public MerchantEntity getMerchantById(@RequestParam @Valid Integer id){
+        return merchantService.getMerchantById(id);
+    }
 
 
 
