f0404b1f98ce839fa6d8da222693e518c7875a99
AmitKumar1827 <amit.rajput.1827@gmail.com>
2022-03-20 16:20:41 +0530

    merchant added

--- src/main/java/com/quickClick/demo/Service/MerchantImpl.java
+++ src/main/java/com/quickClick/demo/Service/MerchantImpl.java
@@ -1,4 +1,23 @@
 package com.quickClick.demo.Service;
 
-public class MerchantImpl {
+import com.quickClick.demo.Entity.MerchantEntity;
+import com.quickClick.demo.Repository.MerchantRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MerchantImpl implements MerchantService {
+
+    @Autowired
+    MerchantRepo merchantRepo;
+
+    @Override
+    public MerchantEntity getMerchantById(Integer id) {
+        Optional<MerchantEntity> optionalMerchantEntity = merchantRepo.findById(id);
+        MerchantEntity merchantEntity = optionalMerchantEntity.get();
+
+        return merchantEntity;
+    }
 }
